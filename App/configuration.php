c1e4f8e12c63f6fcc23871766943ae542ac1c894
Maksim Kovylov <mega.kovylov@gmail.com>
2020-06-24 21:35:56 +0300

    + configuration.php

---
+++ App/configuration.php
@@ -0,0 +1,12 @@
+<?php
+return
+    [
+        'db' =>
+            [
+                'db' => 'pgsql',
+                'host' => 'localhost',
+                'dbname' => 'profit',
+                'username' => 'profit',
+                'password' => 'profit'
+            ],
+    ];
